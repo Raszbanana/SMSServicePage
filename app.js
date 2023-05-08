76e58a54b8f4ea6bbf7e71a57beb2b29b4e4e11c
Raszbanana <niko.hochloff@gmail.com>
2023-05-08 13:51:56 +0200

    firstcommit

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const apiKey = process.env.API_KEY;
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/send-sms', (req, res) => {
+  const phoneNumber = req.body.phoneNumber;
+  console.log(phoneNumber);
+  const message = req.body.message;
+
+  const formData = new URLSearchParams();
+
+  console.log(apiKey);
+
+  formData.append('user_api_key', apiKey);
+  formData.append('sms_message', message);
+  formData.append('sms_to_phone', phoneNumber);
+
+  const request = {
+    method: 'POST',
+    body: formData,
+  };
+
+  fetch('https://fiotext.com/send-sms', request)
+    .then((response) => {
+      if (response.ok) {
+        res.send('SMS sent!');
+      } else {
+        res.status(500).send('Failed to send SMS');
+      }
+    })
+    .catch((error) => {
+      res.status(500).send('Failed to send SMS');
+    });
+});
+
+app.listen(3000, () => {
+  console.log('SMS app listening on port 3000!');
+});
